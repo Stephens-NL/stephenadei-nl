0a887d017217d78bd0d6d467f4cc9b70374868df
Stephen Adei <s.adei@outlook.com>
2025-01-12 23:02:44 +0100

    Fixed more sentry errors

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,12 +1,8 @@
 'use client';
 
-import NextError from 'next/error';
-
 export default function GlobalError({
-  error,
   reset,
 }: {
-  error: Error & { digest?: string };
   reset: () => void;
 }) {
   return (
